85cd7b43409d5d6d9d90214f987ee826eb210134
barca <tharden@wesleyan.edu>
2014-09-13 14:46:50 -0400

    going over sentament analysis

--- newspun/lib/algorithms/sentament_analysis.py
+++ newspun/lib/algorithms/sentament_analysis.py
@@ -3,10 +3,10 @@ import json
 json_data = open('../wordlist/words.json').read()
 lib = json.loads(json_data)
 class scored_words:
-  def __init___(self):
-    self.score
-    self.comparitave
-    self.words
+  def __init___(self,score,comparitave,words):
+    self.score = score
+    self.comparitave = comparitave
+    self.words = words
 def change_score(tally,phrase,is_neg):
     no_punctuation = re.sub('/[^a-zA-Z]+/g', " ",phrase)
     no_punctuation = re.sub('/ {2, }/', ' ',no_punctuation)
@@ -28,10 +28,11 @@ def add_append(t,score,tally,l):
   return tally
 
 def analyze_get_score(phrase,need_score):
-  tally = scored_word(0,0,[])
+  tally = scored_words(0,0,[])
   tally = change_score(tally,phrase,True)
   tally = change_score(tally,phase,False)
   if(need_score):
     return tally.score
   else:
     return tally.comparitave
+print(analyze_get_score('i hate this shit',False))
