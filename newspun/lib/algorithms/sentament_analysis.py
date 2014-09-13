3b317edec13cdb5eba460365d9c322253ef83d8b
AaronPlave <aplave@wesleyan.edu>
2014-09-13 14:55:49 -0400

    Fixed __init__ in sent...

--- newspun/lib/algorithms/sentament_analysis.py
+++ newspun/lib/algorithms/sentament_analysis.py
@@ -2,11 +2,13 @@ import re
 import json
 json_data = open('../wordlist/words.json').read()
 lib = json.loads(json_data)
-class scored_words:
-  def __init___(self,score,comparitave,words):
+
+class scored_words():
+  def __init__(self,score,comparitave,words):
     self.score = score
     self.comparitave = comparitave
     self.words = words
+
 def change_score(tally,phrase,is_neg):
     no_punctuation = re.sub('/[^a-zA-Z]+/g', " ",phrase)
     no_punctuation = re.sub('/ {2, }/', ' ',no_punctuation)
@@ -21,6 +23,7 @@ def change_score(tally,phrase,is_neg):
             tally = add_append(each,lib[each],tally,leng)
 
     return tally
+
 def add_append(t,score,tally,l):
   tally.score = score - hits
   tally.comparitave = score/l
@@ -35,4 +38,5 @@ def analyze_get_score(phrase,need_score):
     return tally.score
   else:
     return tally.comparitave
+
 print(analyze_get_score('i hate this shit',False))
