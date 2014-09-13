7fffff9c252210515e554ec3f93b86fb9663db25
barca <tharden@wesleyan.edu>
2014-09-13 01:37:54 -0400

    changing __init__.py

--- newspun/__init__.py
+++ newspun/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import render_template
-from lib.word import wordfreq
+from lib.word.wordfreq import frequency
 from lib.media_lib import media
 app = Flask(__name__)
 
 # Initialize blueprints
-app.register_blueprint(wordfreq,url_prefix='/frequency')
+app.register_blueprint(frequency,url_prefix='/frequency')
 
 @app.route('/')
 def index():
