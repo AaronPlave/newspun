e100b0c95be0bd9a45be6cfc8c3ddcb2303ad9e5
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-09-14 04:28:29 -0400

    front end :expressionless:

--- newspun/static/app.js
+++ newspun/static/app.js
@@ -12,18 +12,14 @@ $(window).scroll(function() {
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    // $('a.page-scroll').bind('click', function(event) {
-    //     var $anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: $($anchor.attr('href')).offset().top
-    //     }, 500, 'easeInOutExpo');
-    //     event.preventDefault();
-    // });
-
+$.getJSON('/sources', function(sources) {
+  console.log(sources);
   var engine = new Bloodhound({
     name: 'sources',
-    local: [{ name: 'NBC' }, { name: 'Washington Post' }, { name: 'New York Times' }],
+    // local: [{ name: 'NBC' }, { name: 'Washington Post' }, { name: 'New York Times' }],
+    local: sources.map(function (item) {
+      return {name: item.val};
+    }),
     datumTokenizer: function(d) {
       return Bloodhound.tokenizers.whitespace(d.name);
     },
@@ -31,7 +27,7 @@ $(function() {
   });
 
   engine.initialize();
-  $('input').tagsinput({
+  $('#typeahead').tagsinput({
     typeaheadjs: {
       name: 'sources',
       displayKey: 'name',
@@ -65,13 +61,22 @@ var AppRouter = Backbone.Router.extend ({
       started = true;
       // Intro route
       $('section').hide();
+      $('#option-proximity').hide();
       $('.navbar-nav li').hide();
       $('.navbar-nav li:eq(0)').show();
       $('.selected').removeClass('selected');
     },
     'sources': function () {
       if(!started) {this.navigate('',{trigger:true});return;}
+
+      var type = $('header .selected').parent().data('code');
+
       // Sources route
+
+      if(type === "proximity") {
+        $('#option-proximity').show();
+      }
+
       $('section#sources').show();
       $('.navbar-nav li').hide();
       $('.navbar-nav li:eq(0)').show();
@@ -86,11 +91,14 @@ var AppRouter = Backbone.Router.extend ({
         this.navigate('sources',{trigger: true});
         return;
       }
+      var type = $('header .selected').parent().data('code');
+      var words = $('#option-proximity input').map(function(){return $(this).val();});
+      if(type === "proximity" && (words[0] === "" || words[1] === "")) {
+        this.navigate('sources',{trigger: true});
+      }
       // Graphs route
       $('section#graphs').show();
       $('.navbar-nav li:eq(2)').show();
-
-      var type = $('header .selected').parent().data('code');
 
       $('#graph-location').html('');
       switch (type) {
@@ -99,15 +107,22 @@ var AppRouter = Backbone.Router.extend ({
             frequency(data, items);
           });
           break;
+        case 'sentiment':
+          $.getJSON('/', function () {
 
+          });
+        case 'readability':
+          $.getJSON('/', function () {
+
+          });
+        case 'proximity':
+          $.getJSON('/', function () {
+
+          });
         default:
           this.navigate('', {trigger: true});
 
       }
-
-
-
-
       goToSection();
     },
     '*x': function () {
